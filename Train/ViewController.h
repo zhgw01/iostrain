1facc1657b74a28f4b7c881ffbadc64f72a54ded
Gongwei Zhang <zhgw01@gmail.com>
2013-10-09 19:46:47 +0800

    Initial Commit

---
+++ Train/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Train
+//
+//  Created by Zhang Gongwei on 10/9/13.
+//  Copyright (c) 2013 Zhang Gongwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TrainTests/TrainTests.m
@@ -0,0 +1,34 @@
+//
+//  TrainTests.m
+//  TrainTests
+//
+//  Created by Zhang Gongwei on 10/9/13.
+//  Copyright (c) 2013 Zhang Gongwei. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TrainTests : XCTestCase
+
+@end
+
+@implementation TrainTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
