a18065f61857c00a8e9c8b0e1c978da4ceb26023
Gongwei Zhang <zhgw01@gmail.com>
2013-10-11 21:18:30 +0800

    add table view page

---
+++ Train/ExampleListController.h
@@ -0,0 +1,20 @@
+//
+//  ExampleListController.h
+//
+//  Reference: http://www.techotopia.com/index.php/Using_Xcode_Storyboards_to_Build_Dynamic_TableViews_with_Prototype_Table_View_Cells
+//
+//  Created by Zhang Gongwei on 10/9/13.
+//  Copyright (c) 2013 Zhang Gongwei. All rights reserved.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ExampleListController : UITableViewController
+{
+}
+
+@property (nonatomic, strong) NSArray *examples;
+
+@end
---
+++ Train/ExampleListController.m
@@ -0,0 +1,127 @@
+//
+//  ExampleListController.m
+//  Train
+//
+//  Created by Zhang Gongwei on 10/9/13.
+//  Copyright (c) 2013 Zhang Gongwei. All rights reserved.
+//
+
+#import "ExampleListController.h"
+
+@interface ExampleListController ()
+
+@end
+
+@implementation ExampleListController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    //return [self.examples count];
+    return 3;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"CustomCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    
+    UILabel *titleLabel = (UILabel *)[cell viewWithTag:102];
+    titleLabel.text = @"Demo Title";
+    
+    UILabel *rateLabel = (UILabel *) [cell viewWithTag:103];
+    rateLabel.text = @"description";
+    
+    UILabel *priceLabel = (UILabel *) [cell viewWithTag:104];
+    priceLabel.text = @"150";
+    
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a story board-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+ */
+
+@end
