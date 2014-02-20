21f102b4067584b8b99c9e33d528eb7ad55a35eb
Gongwei Zhang <zhgw01@gmail.com>
2014-02-20 21:25:43 +0800

    Implement Basic Data Structure

---
+++ Train/Map/LargeData/TBMapViewController.h
@@ -0,0 +1,16 @@
+//
+//  TBMapViewController.h
+//  Train
+//
+//  Created by Zhang Gongwei on 2/17/14.
+//  Copyright (c) 2014 Zhang Gongwei. All rights reserved.
+//
+//  Tutorial on http://robots.thoughtbot.com/how-to-handle-large-amounts-of-data-on-maps
+//  github on https://github.com/thoughtbot/TBAnnotationClustering/
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TBMapViewController : UIViewController
+
+@end
---
+++ Train/Map/LargeData/TBMapViewController.m
@@ -0,0 +1,38 @@
+//
+//  TBMapViewController.m
+//  Train
+//
+//  Created by Zhang Gongwei on 2/17/14.
+//  Copyright (c) 2014 Zhang Gongwei. All rights reserved.
+//
+
+#import "TBMapViewController.h"
+
+@interface TBMapViewController ()
+
+@end
+
+@implementation TBMapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
