593120589eebf70e85a9a7947b85d8706f2367d6
Gongwei Zhang <zhgw01@gmail.com>
2014-09-23 20:51:46 +0800

    application state transfer

--- Train/AppDelegate.m
+++ Train/AppDelegate.m
@@ -24,6 +24,7 @@ int ddLogLevel = LOG_LEVEL_WARN;
     // Override point for customization after application launch.
     [DDLog addLogger:[DDTTYLogger sharedInstance]];
     DDLogVerbose(@"starting application");
+    // Main Storyboard is instantiated in UIApplicationMain
     return YES;
 }
 							
@@ -31,27 +32,37 @@ int ddLogLevel = LOG_LEVEL_WARN;
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    DDLogInfo(@"Resign active");
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    DDLogInfo(@"Enter background");
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    DDLogInfo(@"Enter foreground");
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    DDLogInfo(@"Become Active");
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    DDLogInfo(@"Terminate");
+}
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+    DDLogInfo(@"URL %@ from %@", url, sourceApplication);
+    return YES;
 }
 
 @end
